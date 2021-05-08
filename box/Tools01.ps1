1db159bcb526f23e264426e58d5271c040c2e9d6
aw <artur.wojciechowicz@interia.pl>
2021-05-08 23:08:26 +0200

    ,,

--- box/Tools01.ps1
+++ box/Tools01.ps1
@@ -1,3 +1,9 @@
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 choco install -y 7zip.install
-choco install -y sysinternals
+choco install notepadplusplus
+choco install vcredist140
+choco install sql-server-management-studio
+choco install microsoft-windows-terminal
+choco install visualstudio2019buildtools
+
+
