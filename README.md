7e9d5a0695ad79164ea791c5e14f5288d2eb8412
Artur Wojciechowicz <artur.wojciechowicz@interia.pl>
2021-05-08 21:19:43 +0200

    no message

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 
 |Click link to run  |Description  |
 |---------|---------|
-|<a href='http://boxstarter.org/package/url?https://raw.githubusercontent.com/sftwrpro/ytspace/main/box/CommonDevTools.ps1?token=ASVQULKSUYUWRBOX565KVFDAS3HKA'>Tools</a>     | dev tools |
+|<a href='http://boxstarter.org/package/url?https://raw.githubusercontent.com/sftwrpro/ytspace/main/box/Perceived.ps1'>Tools</a>     | dev tools |
---
+++ box/Perceived.ps1
@@ -0,0 +1,6 @@
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_Classes_Root
+Set-ItemProperty -Path HKCR:\.json -Name PerceivedType -Value "text"
+Set-ItemProperty -Path HKCR:\.config -Name PerceivedType -Value "text"
+Set-ItemProperty -Path HKCR:\.log -Name PerceivedType -Value "text"
+Set-ItemProperty -Path HKCR:\.xml -Name PerceivedType -Value "text"
+Set-ItemProperty -Path HKCR:\.xsl -Name PerceivedType -Value "text"
